ab9681b0d5083caf7de8f6f3e98d690d9524d7eb
Sergey Plotnikov <sergey@plotnikov.kz>
2022-03-28 11:56:39 +0600

    Create module Article

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import ormconfig from '@app/ormconfig'
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+	imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
